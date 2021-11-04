fd623b22dc29196ff85aca2a67e690b82ddb566b
Simon Willcock <swillcock@atlassian.com>
2021-11-04 17:18:46 +1100

    Align with input and output of the lesson
    Coordinates are now from bottom-left

--- src/app.ts
+++ src/app.ts
@@ -10,35 +10,40 @@ interface RGBA {
   a: number
 }
 
-type DrawingFunction = (coords: Coordinates, rgba: RGBA) => ImageData
-type DrawingFactory = (width: number, canvasData: ImageData) => DrawingFunction;
+type DrawingFunction = (coords: Coordinates, rgba: RGBA) => void
+type DrawingFactory = (width: number, height: number, canvasData: ImageData) => DrawingFunction;
 
-const drawingFactory: DrawingFactory = (width, canvasData) => ({x, y}, rgba): ImageData => {
+const drawingFactory: DrawingFactory = (width, height, canvasData) => (coords, rgba) => {
   // Canvas data is an array with one value for each component in a pixel, 
   // organized left to right, top to bottom, with each pixel represented 
   // as four values in RGBA order.
-  const pixelIndex = (x + y * width) * 4;
+  
+  // We will be passing in coordinates assuming the origin is at the bottom left,
+  // so we need to translate them accordingly
+  const { x, y } = coords;
+  const translatedY = Math.abs(height - y);
+
+  const pixelIndex = (translatedY * width + x) * 4;
 
   // Set RGBA values as each component of the pixel
   Object.values(rgba).forEach((value: number, offset) => {
     canvasData.data[pixelIndex + offset] = value;
   });
-  return canvasData;
 }
 
 const renderer = (selector: string) => {
   const canvas = document.querySelector(selector) as HTMLCanvasElement;
-  const { width } = canvas;
+  const { width, height } = canvas;
   const ctx = canvas.getContext('2d');
 
   // Always start with a blank canvas
   let canvasData = ctx.createImageData(canvas.width, canvas.height);
 
   // Create drawing function
-  const drawPixel = drawingFactory(width, canvasData);
+  const drawPixel = drawingFactory(width, height, canvasData);
 
-  // Do drawing
-  canvasData = drawPixel({x: 75, y: 75}, {r: 255, g: 0, b: 255, a: 255});
+  // Do drawing - simple for now
+  drawPixel({x: 52, y: 41}, {r: 255, g: 0, b: 0, a: 255});
 
   // Update the canvas with the new data
   ctx.putImageData(canvasData, 0, 0)
